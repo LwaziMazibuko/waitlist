8dd8008996266134430a48d0ea20ba981fdeb375
Lwazi Mazibuko <luyandamazibuko84@gmail.com>
2024-04-28 01:44:59 +0200

    Back before route

--- apps/nextjs/next.config.mjs
+++ apps/nextjs/next.config.mjs
@@ -1,15 +1,10 @@
 // @ts-check
-import { config as dotenvConfig } from "dotenv";
-dotenvConfig();
+import "./src/env.mjs";
 
-// Only import and validate environment variables in development
-if (process.env.NODE_ENV !== "production") {
-  !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
-}
+!process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
 const config = {
-  basePath: '/waitlist', // Set the base path to '/waitlist'
   reactStrictMode: true,
   /** Enables hot reloading for local packages without a build step */
   transpilePackages: ["@saasfly/common", "@saasfly/ui"],
@@ -21,4 +16,3 @@ const config = {
 };
 
 export default config;
-
